e45b36fe863a28807bb0021e7df45f6ebd1ed5f7
czross <czr@uw.edu>
2016-10-09 18:52:23 -0700

    Setting up

---
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Brian Ross on 10/9/16.
+//  Copyright © 2016 Chris Ross. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
